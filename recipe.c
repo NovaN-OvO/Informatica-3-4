81b30e39859aca4957d9c2551db1d2fb6d6160a4
NovaN-OvO <gato.punteado@gmail.com>
2026-04-20 14:58:49 +0000

    new recipe progress

--- recipe.c
+++ recipe.c
@@ -3,11 +3,10 @@
 int main(void)
 {
     printf("Coconut Cranberry Dessert \n");
-    printf("1 1/2 cups\tgraham cracker crumbs (about 24 full-sized graham crackers \n)");
     printf("\n");
+    printf("1 1/2 cups\tgraham cracker crumbs (about 24 full-sized graham crackers) \n");
     printf("1/2 cup\tbutter or margarine, melted \n");
-    printf("1/2 cup\tbutter or margarine, melted \n");
-    printf("1 can (12 ounces/340 g)\tsweetened condensed milk \n");
+    printf("1 can\tsweetened condensed milk \n");
     printf("1 cup\tflaked coconut \n");
     printf("1 cup\tdried cranberries \n");
     printf("2 cups\tvanilla chips \n");
