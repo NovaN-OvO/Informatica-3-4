d73d0a46fcf98e20e69656cf0e42e908970bf272
NovaN-OvO <gato.punteado@gmail.com>
2026-04-21 14:54:12 +0000

    Naming variables, commenting, and naming errors.

---
+++ naming-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int number; //I changed the variable and deleted the exclamation mark.
+int test_value; //I added an underscore instead of a space.
+int Example1; //I changed the position of the letters and the number.
+float int_variable; // I deleted the "int" variable and changed it into another valid variable.
+printf("Variables created successfully.\n");
+}
--- recipe.c
+++ recipe.c
@@ -14,4 +14,6 @@ int main(void)
     printf("\n");
     printf("1. \t\t Combine graham cracker crumbs and butter. Press into a greased 13″ x 9″ (33 x 23-cm) pan. \n");
     printf("2. \t\t Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares. \n");
+    printf("\n");
+    printf("\033[1;35mENJOY! :)\033[0m \n");
 }
