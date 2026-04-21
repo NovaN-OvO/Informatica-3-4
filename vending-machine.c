839473b972f78dace58720a015df3370f378920e
NovaN-OvO <gato.punteado@gmail.com>
2026-04-21 15:49:48 +0000

    Vending machine start

---
+++ vending-machine.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //Separate the variables from the prints.
+    //Chips
+    int Quantity_Chips = 20;
+    float Price1 = 1.50;
+    char Selection1 = 'A'; //You have to put (') between the value.
+
+    //Soda
+    int Quantiity_Soda = 15;
+    float Price2 = 2.00;
+    char Selection2 = 'B';
+
+    //Candy
+
+    //Prints
+    printf("Vending Machine Stock \n");
+    printf("----------------------------\n");
+    printf("Item Name: Chips \n");// Add the backwards slash.
+    printf("Price: $%f" , 'Price1' );
+}
