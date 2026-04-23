2db5a500bb45b58b10013c442d8a4e7a0730e90f
NovaN-OvO <gato.punteado@gmail.com>
2026-04-23 21:09:18 +0000

    Input practice

--- hello.c
+++ hello.c
@@ -2,5 +2,10 @@
 
 int main(void)
 {
-    printf("Hello Student Lauro! \n");
+    printf("Enter your name and your favorite color: ");
+    char name[50];
+    char color[50];
+    scanf("%s" , &name); //The "&" sets to save the value to the variable.
+    scanf("%s", &color);
+    printf("Hello %s, Your favorite color is %s!\n", name, color);
 }
