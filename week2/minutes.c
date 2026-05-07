491ae96cc93630b664e57f113de930fc521e1120
NovaN-OvO <gato.punteado@gmail.com>
2026-05-07 14:56:25 +0000

    Fixed minutes

--- week2/minutes.c
+++ week2/minutes.c
@@ -4,22 +4,31 @@ int main(void)
 {
     //USE MODULUS "%" AND IF NOT THEN USE : a - (a / b) * b
     int Minutes;
-    int Start_Time;
 
     printf("Movie running time (in minutes): ");
     scanf("%d", &Minutes);
 
-    printf("\nStart time: ");
-    scanf("%d", &Start_Time);
+    int Start_h;
+    int Start_m;
+    printf("Start time: ");
+    scanf("%d:%d", &Start_h, &Start_m);
 
     int Runtime = Minutes/60;
     int Runtime2 = Minutes%60;
 
-    int Endtime = Start_Time + Runtime;
-    int Endtime2 = Start_Time + Runtime2;
+    // Problem2: 182m > 5:59 >> 8:61
+    int End_h = Start_h + Runtime; // end_h = 8
+    int End_m = Start_m + Runtime2; // end_m = 61
 
-    printf("The run time of the movie is %d hours and %d minutes.\n", Runtime, Runtime2);
-    printf("You will finish the movie at %d:%d.\n", Endtime, Endtime2);
+    // This will update the variable value
+    if (End_m >= 60){
+        End_h = End_h + (End_m / 60);
+        End_m = End_m % 60; // The remainder of the 61 minutes by 60 minutes is equal to 1 and it will show the remainder (n:01) instead of 61
+
+    }
+
+    printf("\nThe run time of the movie is %d hours and %d minutes.\n", Runtime, Runtime2);
+    printf("You will finish the movie at %d:%02d\n", End_h, End_m);
 
 
 }
